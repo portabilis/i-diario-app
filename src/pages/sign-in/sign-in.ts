26a8cff4feb91c6be84a2a43d432370d9156da04
Caroline Salib <carolinesalibc@gmail.com>
2017-07-01 15:04:16 -0300

    Alterado layout da página de login; Refs #3

--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -14,7 +14,7 @@ import { User } from '../../data/user.interface';
 
 @Component({
   selector: 'page-sign-in',
-  templateUrl: 'sign-in.html',
+  templateUrl: 'sign-in.html'
 })
 export class SignIn {
   constructor(
