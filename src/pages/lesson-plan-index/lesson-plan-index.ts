c61bbdc6f8ee47f88f8003bb0f11ae4f70ee4e5c
Caroline Salib <carolinesalibc@gmail.com>
2017-07-11 19:48:02 -0300

    Ajustado index de planos de aula para pegar dados da api; Refs #14

--- src/pages/lesson-plan-index/lesson-plan-index.ts
+++ src/pages/lesson-plan-index/lesson-plan-index.ts
@@ -1,7 +1,10 @@
+import { ConnectionService } from './../../services/connection';
+import { Storage } from '@ionic/storage';
 import { AuthService } from './../../services/auth';
 import { LessonPlansService } from './../../services/lesson_plans';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -11,35 +14,67 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class LessonPlanIndexPage {
   shownGroup = null;
 
-  lessons = [];
+  plans = [];
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               private auth: AuthService,
-              private lessonPlansService: LessonPlansService) {
+              private lessonPlansService: LessonPlansService,
+              private storage: Storage,
+              private toastCtrl: ToastController,
+              private connectionService: ConnectionService) {
   }
-  // ionViewDidLoad() {
-  //   atualizar lessonplans offline
-  // }
 
-  updateLessonPlans(refresher) {
+  ionViewDidLoad() {
+    this.updateLessonPlans();
+  }
+
+  doRefresh(refresher) {
     this.auth.currentUser().then((user) => {
       this.lessonPlansService.getLessonPlans(
         user.teacher_id
       ).subscribe(
-        (lessonPlans:any) => {
-          lessonPlans.forEach(lessonPlan => {
-            this.lessons = [{ unity: lessonPlan.unity_name,
-                              disciplines: [lessonPlan.description + ' - ' + lessonPlan.classroom_name] }]
-          });
+        (lesson_plans:any) => {
+          this.storage.set('lesson_plans', lesson_plans);
+          this.updateLessonPlans();
         },
         (error) => {
-          console.log(error);
+          this.presentErrorToast();
+          refresher.cancel();
         },
         () => {
           refresher.complete();
         }
       );
+    });
+  }
+
+  presentErrorToast() {
+    let offlineMessage = "";
+
+    if(!this.connectionService.isOnline){
+      offlineMessage = " Parece que você está offline";
+    }
+
+    let toast = this.toastCtrl.create({
+      message: 'Não foi possível completar a atualização.' + offlineMessage,
+      duration: 3000,
+      position: 'down'
+    });
+
+    toast.present();
+  }
+
+  updateLessonPlans() {
+    this.storage.get('lesson_plans').then((lesson_plans) => {
+    this.plans = [];
+      lesson_plans.unities.forEach(unity => {
+        let plan_disciplines = [];
+        unity.plans.forEach(plan => {
+          plan_disciplines.push(plan.description + ' - ' + plan.classroom_name);
+        });
+        this.plans.push({ unity: unity.unity_name, disciplines: plan_disciplines});
+      });
     });
   }
 
