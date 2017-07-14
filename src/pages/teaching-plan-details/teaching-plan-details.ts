1a732de8fb03a044c019e65771d964c2f493da65
Caroline Salib <carolinesalibc@gmail.com>
2017-07-14 16:02:19 -0300

    Adicionado tela de detalhes do plano de ensino; Refs #21

--- src/pages/teaching-plan-details/teaching-plan-details.ts
+++ src/pages/teaching-plan-details/teaching-plan-details.ts
@@ -1,24 +1,56 @@
+import { Storage } from '@ionic/storage';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the TeachingPlanDetailsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-teaching-plan-details',
   templateUrl: 'teaching-plan-details.html',
 })
 export class TeachingPlanDetailsPage {
+  teachingPlanId: number;
+  description: string;
+  unity_name: string;
+  period: string;
+  objectives: string;
+  activities: string;
+  evaluation: string;
+  bibliography: string;
+  contents: string;
+  knowledge_areas: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private storage: Storage) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TeachingPlanDetailsPage');
+    this.teachingPlanId = this.navParams.get('teachingPlanId');
+
+    this.storage.get('teaching_plans').then((teaching_plans) => {
+      let details = this.getTeachingPlanDetail(teaching_plans);
+      this.description = details.description + ' - ' + details.grade_name;
+      this.unity_name = details.unity_name;
+      this.period = details.period;
+      this.activities = details.activities;
+      this.evaluation = details.evaluation;
+      this.bibliography = details.bibliography;
+      this.contents = details.contents;
+      this.knowledge_areas = details.knowledge_areas;
+    });
+  }
+
+  getTeachingPlanDetail(teachingPlan){
+    let response;
+    teachingPlan.unities.forEach(unity => {
+      unity.plans.forEach(plan => {
+        if (plan.id == this.teachingPlanId) {
+          response = plan;
+          return;
+        }
+      })
+    });
+    return response;
   }
 
 }
