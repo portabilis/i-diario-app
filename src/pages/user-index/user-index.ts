d47631953aa950cb407a19fde482ae88adedafbb
Caroline Salib <carolinesalibc@gmail.com>
2017-08-09 17:57:26 -0300

    Adicionado mensagem ao fazer logout com dados a serem sincronizados; Refs #55

--- src/pages/user-index/user-index.ts
+++ src/pages/user-index/user-index.ts
@@ -1,5 +1,6 @@
+import { Storage } from '@ionic/storage';
 import { SignIn } from './../sign-in/sign-in';
-import { App } from 'ionic-angular';
+import { App, AlertController } from 'ionic-angular';
 import { AuthService } from './../../services/auth';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -11,15 +12,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UserIndexPage {
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private auth: AuthService,
-              private app: App) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private auth: AuthService,
+    private app: App,
+    private storage: Storage,
+    private alertCtrl: AlertController
+  ) {}
 
   exit() {
+    this.storage.get('dailyFrequencyStudentsToSync').then((dailyFrequencyStudentsToSync) => {
+      if (dailyFrequencyStudentsToSync) {
+        this.showConfirmExit();
+      } else {
+        this.logout();
+      }
+    });
+  }
+
+  showConfirmExit() {
+    let confirm = this.alertCtrl.create({
+      title: 'Deseja realmente sair?',
+      message: 'Encontramos alguns lançamentos que ainda não foram sincronizados.',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {}
+        },
+        {
+          text: 'Apagar lançamentos e sair',
+          handler: () => {
+            this.logout();
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  logout() {
     this.auth.removeCurrentUser();
     this.app.getRootNav().setRoot(SignIn);
   }
-
 }
