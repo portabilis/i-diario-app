39289e525312daa0a6d7011ae1b922d5865449f6
Gabriel Souza <gabrielmatosdesouza@gmail.com>
2017-08-18 11:02:50 -0300

    Adicionada sincronização de alunos; Refs #66

---
+++ src/services/offline_data_persistence/students_persister.ts
@@ -0,0 +1,37 @@
+import { StudentsService } from './../students';
+import { ClassroomsService } from './../classrooms';
+import { Observable } from 'rxjs/Observable';
+import { Storage } from '@ionic/storage';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class StudentsPersisterService{
+  constructor(
+    private classrooms: ClassroomsService,
+    private storage: Storage,
+    private students: StudentsService
+  ){}
+
+  persist(user, disciplines){
+   return new Observable((observer) => {
+      let studentsObservables = []
+      disciplines.forEach((disciplineList) => {
+        disciplineList.data.forEach((discipline) => {
+          studentsObservables.push(this.students.getStudents(disciplineList.classroomId, discipline.id))
+        });
+      })
+
+      Observable.forkJoin(studentsObservables).subscribe(
+        (results: any) => {
+          this.storage.set('students', results)
+        },
+        (error) => {
+          observer.error(error);
+        },
+        () => {
+          observer.complete()
+        }
+      )
+    })
+  }
+}
