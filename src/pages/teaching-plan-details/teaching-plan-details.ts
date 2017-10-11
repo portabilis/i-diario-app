dc94c72d31e1ffdd9379fd49e32a35e0dae93e16
Caroline Salib <carolinesalibc@gmail.com>
2017-10-11 08:46:20 -0300

    Ajustado para trazer os textos de forma correta; Refs #21

--- src/pages/teaching-plan-details/teaching-plan-details.ts
+++ src/pages/teaching-plan-details/teaching-plan-details.ts
@@ -1,3 +1,4 @@
+import { UtilsService } from './../../services/utils';
 import { Storage } from '@ionic/storage';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -19,10 +20,12 @@ export class TeachingPlanDetailsPage {
   contents: string;
   knowledge_areas: string;
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private storage: Storage) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private storage: Storage,
+    private utilsService: UtilsService
+  ) {}
 
   ionViewDidLoad() {
     this.teachingPlanId = this.navParams.get('teachingPlanId');
@@ -32,12 +35,13 @@ export class TeachingPlanDetailsPage {
       this.description = details.description + ' - ' + details.grade_name;
       this.unity_name = details.unity_name;
       this.period = details.period;
-      this.objectives = details.objectives;
-      this.activities = details.activities;
-      this.evaluation = details.evaluation;
-      this.bibliography = details.bibliography;
       this.contents = details.contents;
       this.knowledge_areas = details.knowledge_areas;
+
+      this.objectives = this.utilsService.convertTextToHtml(details.objectives);
+      this.evaluation = this.utilsService.convertTextToHtml(details.evaluation);
+      this.bibliography = this.utilsService.convertTextToHtml(details.bibliography);
+      this.activities = this.utilsService.convertTextToHtml(details.activities);
     });
   }
 
