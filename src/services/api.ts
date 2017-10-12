55a31ca9711f3ec8ed6c037a02a55691112c9086
Lucas Schmoeller da Silva <lucasschmoellerdasilva@gmail.com>
2017-10-12 14:17:07 -0300

    Atualizada urls da api para v2
    Refs #83

--- src/services/api.ts
+++ src/services/api.ts
@@ -76,48 +76,48 @@ export class ApiService {
   }
 
   getTeatcherClassroomsUrl() {
-    return this.getServerUrl() + '/api/v1/teacher_classrooms.json'
+    return this.getServerUrl() + '/api/v2/teacher_classrooms.json'
   }
   getLoginUrl() {
     return this.getServerUrl() + '/usuarios/logar.json'
   }
   getDailyFrequencyStudentsUrl(id: number) {
-    return this.getServerUrl() + '/api/v1/daily_frequency_students/' + id + '.json';
+    return this.getServerUrl() + '/api/v2/daily_frequency_students/' + id + '.json';
   }
   getDailyFrequencyUrl() {
-    return this.getServerUrl() + '/api/v1/diario-de-frequencia.json'
+    return this.getServerUrl() + '/api/v2/diario-de-frequencia.json'
   }
   getTeacherDisciplinesUrl() {
-    return this.getServerUrl() + '/api/v1/teacher_disciplines.json'
+    return this.getServerUrl() + '/api/v2/teacher_disciplines.json'
   }
   getExamRulesUrl() {
-    return this.getServerUrl() + '/api/v1/exam_rules.json'
+    return this.getServerUrl() + '/api/v2/exam_rules.json'
   }
   getSchoolCalendarUrl() {
-    return this.getServerUrl() + '/api/v1/calendarios-letivo.json'
+    return this.getServerUrl() + '/api/v2/calendarios-letivo.json'
   }
   getClassroomStudentsUrl() {
-    return this.getServerUrl() + '/api/v1/classroom_students.json'
+    return this.getServerUrl() + '/api/v2/classroom_students.json'
   }
   getTeacherUnitiesUrl() {
-    return this.getServerUrl() + '/api/v1/teacher_unities.json'
+    return this.getServerUrl() + '/api/v2/teacher_unities.json'
   }
   getTeacherLessonPlansUrl() {
-    return this.getServerUrl() + '/api/v1/lesson_plans.json'
+    return this.getServerUrl() + '/api/v2/lesson_plans.json'
   }
   getContentLessonPlansUrl() {
-    return this.getServerUrl() + '/api/v1/content_records/lesson_plans.json'
+    return this.getServerUrl() + '/api/v2/content_records/lesson_plans.json'
   }
   getContentRecordsUrl() {
-    return this.getServerUrl() + '/api/v1/content_records.json'
+    return this.getServerUrl() + '/api/v2/content_records.json'
   }
   getTeacherTeachingPlansUrl() {
-    return this.getServerUrl() + '/api/v1/teaching_plans.json'
+    return this.getServerUrl() + '/api/v2/teaching_plans.json'
   }
   getDailyFrequencyStudentsUpdateOrCreateUrl() {
-    return this.getServerUrl() + '/api/v1/daily_frequency_students/update_or_create.json'
+    return this.getServerUrl() + '/api/v2/daily_frequency_students/update_or_create.json'
   }
   getContentRecordsSyncUrl(){
-    return this.getServerUrl() + '/api/v1/content_records/sync.json'
+    return this.getServerUrl() + '/api/v2/content_records/sync.json'
   }
 }
