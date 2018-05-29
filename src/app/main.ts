0d12d21deaae3d9d8f2cfa4a24234aba022939da
Wilkinson Souza <wilkinson@outlook.com.br>
2018-05-29 11:09:55 -0300

    Adiciona para que o Angular rode em produção como padrão.
    Aumenta a espera em dispositivos android para verificar o tempo de resposta.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
 import './polyfills.ts';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { environment } from './../environments/environment';
+import { AppModule } from './app.module';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
