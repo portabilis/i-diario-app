ebbcc54b0b0eb7052a96523d473a575bd24fd34a
Wilkinson Souza <wilkinson@outlook.com.br>
2018-08-06 15:06:59 -0300

    Atualiza a versão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { File } from '@ionic-native/file';
 
 Pro.init('***REMOVED***', {
-  appVersion: '0.0.33'
+  appVersion: '0.0.34'
 })
 
 //Pages
