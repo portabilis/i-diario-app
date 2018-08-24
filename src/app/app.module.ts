56635339a41696ee2900c38f2d6e9d70ce59b74e
David Echeli <davidecheli@gmail.com>
2018-08-24 10:40:32 -0300

    Alterado para a versão 38

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { File } from '@ionic-native/file';
 
 Pro.init('***REMOVED***', {
-  appVersion: '0.0.37'
+  appVersion: '0.0.38'
 })
 
 //Pages
