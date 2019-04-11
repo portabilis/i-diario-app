5b04496073bc479e056ec51ab20f7240e7b1739f
Jonatan Potrikus <jonatanpotrikus@gmail.com>
2019-04-11 15:50:53 -0300

    Enviar parametro do professor no sincronismo de frequencia na rota de (diario-frequencia) Refs #131

--- src/services/offline_data_synchronization/daily_frequencies_synchronizer.ts
+++ src/services/offline_data_synchronization/daily_frequencies_synchronizer.ts
@@ -4,50 +4,56 @@ import { Response } from '@angular/http';
 import { Storage } from '@ionic/storage';
 import { Injectable } from '@angular/core';
 import { CustomHttp } from '../custom_http';
+import { AuthService } from '../auth';
 
 @Injectable()
 export class DailyFrequenciesSynchronizer {
   constructor(
     private http: CustomHttp,
     private api: ApiService,
-    private storage: Storage
+    private storage: Storage,
+    private auth: AuthService
   ){}
 
   public sync(dailyFrequencies){
     return new Observable((observer) => {
       if(dailyFrequencies){
-        let dailyFrequencyObservables = []
-        dailyFrequencies.forEach(dailyFrequency => {
-          const request = this.mountDailyFrequencyPostRequest(dailyFrequency)
-          dailyFrequencyObservables.push(request)
-        })
+        this.auth.currentUser().then((user) => {
+          let dailyFrequencyObservables = []
+          dailyFrequencies.forEach(dailyFrequency => {
+            const request = this.mountDailyFrequencyPostRequest(dailyFrequency, user.teacher_id)
+            dailyFrequencyObservables.push(request)
+          })
 
-        Observable.concat(...dailyFrequencyObservables).subscribe(
-          (result) => {
-            observer.next(result)
-          },
-          (error) => {
-            observer.error(error)
-          },
-          () => {
-            observer.complete()
-          }
-        )
+          Observable.concat(...dailyFrequencyObservables).subscribe(
+            (result) => {
+              observer.next(result)
+            },
+            (error) => {
+              observer.error(error)
+            },
+            () => {
+              observer.complete()
+            }
+          )
+        });
       }else{
         observer.complete()
       }
     })
   }
 
-  private mountDailyFrequencyPostRequest(dailyFrequency){
+  private mountDailyFrequencyPostRequest(dailyFrequency, teacherId){
     return this.http.post(this.api.getDailyFrequencyUrl(), {
-      unity_id: dailyFrequency.unity_id,
-      classroom_id: dailyFrequency.classroom_id,
-      frequency_date: dailyFrequency.frequency_date,
-      discipline_id: dailyFrequency.discipline_id,
-      class_number: dailyFrequency.class_number
-    }).map((response: Response) => {
-      return response.json();
-    });
+        unity_id: dailyFrequency.unity_id,
+        classroom_id: dailyFrequency.classroom_id,
+        frequency_date: dailyFrequency.frequency_date,
+        discipline_id: dailyFrequency.discipline_id,
+        class_number: dailyFrequency.class_number,
+        teacher_id: teacherId
+      }).map((response: Response) => {
+        return response.json();
+      });
+
   }
 }
